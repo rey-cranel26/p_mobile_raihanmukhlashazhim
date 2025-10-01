24615fca2ad48d595cb49b6b135dfb34f9b4f575
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-01 11:34:38 +0700

    komentar

---
+++ bin/komentar.dart
@@ -0,0 +1,13 @@
+// ini documentasi
+//bisa digenerate menjadi dart.doc
+void main() {
+  //ini variabel
+   String nama;
+  nama = 'Raihan Mukhlash azhim';
+
+  /**ini untuk
+   * komentar lebih
+   * satu baris
+   */
+  print(nama);
+}
