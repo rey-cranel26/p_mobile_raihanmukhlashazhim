f986420e7de92d25075cfa6e76aedbf749d4365a
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-01 11:25:35 +0700

    kata_kunci_late

---
+++ bin/kata_kunci_late.dart
@@ -0,0 +1,13 @@
+void main(){
+  
+  late var value = getvalue();
+  print('display value');
+  print(value);
+  }
+
+  String getvalue() {
+    print('getvalue dipanggil');
+    return 'eko kurniawan khannedy';
+
+  }
+  
