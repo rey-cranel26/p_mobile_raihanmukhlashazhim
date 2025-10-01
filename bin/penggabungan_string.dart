65fc43154748c7c8ca5970f7e01b411d567583a0
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-01 17:55:57 +0800

    penggabungan_string

---
+++ bin/penggabungan_string.dart
@@ -0,0 +1,10 @@
+void main() {
+  var firstname = 'Raihan';
+  var lastname = 'Mukhlash azhim';
+
+  var name1 = firstname + ' ' + lastname;  
+  var name2 = 'raihan' ' mukhlash azhim';  
+
+  print(name1); 
+  print(name2); 
+}
