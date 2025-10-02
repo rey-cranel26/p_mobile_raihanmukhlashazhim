a6390123d3e207a14834fcc4632c1cfcaf1cab8d
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-02 16:03:55 +0800

    operatot_aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,20 @@
+void main(){
+var hitung1 = 20+20;
+var hitung2 = 20-2;
+var hitung3 = 20*20;
+var hitung4 = 20/2;
+var hitung5 = 20~/2;
+var hitung6 = 20%3;
+var hitung7 = -20;
+var hitung8 = 20;
+
+print(hitung1);
+print(hitung2);
+print(hitung3);
+print(hitung4);
+print(hitung5);
+print(hitung6);
+print(hitung7);
+print(hitung8);
+
+}
