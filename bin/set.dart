b767690e36dc88a35a46af58a58853b588d88246
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-08 09:09:43 +0800

    set

---
+++ bin/set.dart
@@ -0,0 +1,14 @@
+void main(){
+  Set <String> number ={};
+
+  var names = <String>{};
+
+  final numberdoubel = <String>{};
+
+  names.add('raihan');
+  names.add('mukhlash');
+  names.add('azhim');
+
+  print(names);
+
+}
