65466c24538f12284da1ea276ed80bad5db28534
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-13 02:13:01 +0800

    else_if

---
+++ bin/else_if.dart
@@ -0,0 +1,21 @@
+void main(){
+  var uts =70;
+  var uas =70;
+
+  if(uts>=80 && uas>= 80){
+    print('nilai A');
+
+  } else if(uts>=70 && uas>= 70){
+    print('nilai B');
+
+  } else if(uts>=60 && uas>= 60){
+    print('nilai C');
+
+  } else if(uts>=50 && uas>= 50){
+    print('nilai D');
+ }else {
+   print('anda remedi');
+ 
+}
+
+}
