567f91515998a94e79eb892b15b2dc1365cc5967
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-16 20:55:07 +0800

    switch_case

---
+++ bin/switch_case.dart
@@ -0,0 +1,17 @@
+void main(){
+  var nilai = 'C';
+  switch(nilai){
+    case 'A':
+    print('lulus dengan baik');
+    break;
+    case 'B':
+    case 'C':
+    print('lulus');
+    break;
+    case 'D':
+    print('tidak lulus');
+    break;
+    default:
+    print('salah jurusan');
+  }
+}
