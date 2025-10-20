b7864120354e61a1e55c788c2dfbdc90f6da9a94
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-21 00:52:13 +0800

    Default_Parameter_Value

---
+++ bin/Default_Parameter_Value.dart
@@ -0,0 +1,15 @@
+void sayhello({String? firstname,String? lastname='default'}) {
+print('hello $firstname $lastname');
+
+  
+}
+
+void main() {
+sayhello(firstname:'raihan',lastname:'mukhlash');
+sayhello(lastname:'dian',firstname:'utami');
+sayhello();
+sayhello(lastname:'raihan');
+sayhello(firstname:'raihan');
+
+  
+}
