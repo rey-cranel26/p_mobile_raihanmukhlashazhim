1095b32365b5003e25803f15943a2190793efadb
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-10-24 23:27:54 +0800

    Anonymous_Function_as_variabel

---
+++ bin/Anonymous_Function_as_Variable.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  var upperfunction =(String name){
+    return name.toUpperCase();
+  };
+
+   var lowerfunction =(String name)=> name.toLowerCase();
+
+   print(upperfunction('raihan'));
+   print(lowerfunction('raihan'));  
+
+}
