ef071fd8358420d77d06c1820a3911cbbe720a7d
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-11-11 22:06:42 +0800

    this keyword

---
+++ bin/OOP/this_keyword.dart
@@ -0,0 +1,14 @@
+class person {
+
+  String name= 'rey';
+  String? address;
+  final String country = 'namex';
+
+  person(String name, String address){
+    this.name=name;
+    this.address=address;
+
+    
+  }
+
+}
