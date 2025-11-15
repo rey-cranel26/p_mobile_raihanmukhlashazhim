35db89cdfbdbe875d80cc1b5cefa6729aa128017
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-11-16 00:23:49 +0800

    metadata

---
+++ bin/Membuat Annotation.dart
@@ -0,0 +1,11 @@
+class Todo {
+    final String todo;
+
+    const Todo(this.todo);
+}
+
+class ApplicationLogic {
+@Todo("Will be implemented in next feature")
+     void run() {
+    }
+}
---
+++ bin/Standard Annotation.dart
@@ -0,0 +1,11 @@
+class Sample {
+  @override
+  String toString() {
+    return 'sample';
+  }
+
+  @Deprecated('don\'t use it anymore')
+  void doNotCallMe() {
+    
+  }
+}
