9a0027bff3c773b679e62c5c0cea928544de0046
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-11-15 17:01:14 +0800

    Menggunakan Access Modifier

---
+++ bin/Menggunakan Access Modifier.dart
@@ -0,0 +1,10 @@
+import 'product.dart';
+
+void main(){
+  var product=Product();
+  product.id='2';
+  product.name='legion';
+  // product._quantity=100;
+  print(product.id);
+  print(product.name);
+}
