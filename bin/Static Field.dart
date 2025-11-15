21cde05b1123173ff6b0d86b79197a3f5992881d
rey-cranel26d:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart <raihanraihan317@gmail.comd:\fluter_tugas200\mobile_raihanmukhlashazhim_23031004\bin\hello_word.dart>
2025-11-15 23:10:59 +0800

    Static

---
+++ bin/Static Field.dart
@@ -0,0 +1,12 @@
+class Application{
+  static final String author='ferlin salombo';
+  static final String name='rey cihuyy';
+  
+
+}
+
+void main(){
+  print(Application.name);
+  print(Application.author);
+  
+}
---
+++ bin/Static Method.dart
@@ -0,0 +1,9 @@
+class Math{
+  static int sum(int first,int second)=> first+second;
+
+
+}
+void main(){
+  var result = Math.sum(10,20);
+  print(result);
+}
